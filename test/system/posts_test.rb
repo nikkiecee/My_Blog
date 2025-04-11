8a58051a341f9044a26c0322904b616079d957ba
Nicole E. Eluwa <nicole.eluwa@gmail.com>
2025-04-11 13:17:41 +0100

    fixed issues 2

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -14,7 +14,7 @@ class PostsTest < ApplicationSystemTestCase
     visit posts_url
     click_on "New post"
 
-    fill_in "Body", with: @post.body
+    fill_in "Content", with: @post.content
     fill_in "Title", with: @post.title
     click_on "Create Post"
 
@@ -26,7 +26,7 @@ class PostsTest < ApplicationSystemTestCase
     visit post_url(@post)
     click_on "Edit this post", match: :first
 
-    fill_in "Body", with: @post.body
+    fill_in "Content", with: @post.content
     fill_in "Title", with: @post.title
     click_on "Update Post"
 
